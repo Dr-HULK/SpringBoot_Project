e89712a980eedeacf75a882bf6cad13b0891d746
Dr-HULK <peoplecallmeswarup@gmail.com>
2023-04-14 21:59:11 +0530

    PUT implementation added Major Bugs -	The Throw exception to “Email is already taken" is not working.

--- demo/demo/src/main/java/com/example/demo/student/StudentService.java
+++ demo/demo/src/main/java/com/example/demo/student/StudentService.java
@@ -1,8 +1,10 @@
 package com.example.demo.student;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -31,6 +33,28 @@ public class StudentService {
                     "Student with ID " + studentId + " does not exists.");
         }
         studentRepository.deleteById(studentId);
+    }
+
+    @Transactional
+    public void updateStudent( Long studentId,
+                               String name,
+                               String email){
+        Student student = studentRepository.findById(studentId)
+                .orElseThrow(() -> new IllegalStateException(
+                        "Student with ID " + studentId + " does not exists"));
+
+        if(name != null && name.length() > 0 &&
+            !Objects.equals(student.getName(),name)){
+            student.setName(name);
+        }
+        if(email != null && email.length() > 0 &&
+                !Objects.equals(student.getEmail(),email)){
+            Optional<Student> studentOptional = studentRepository.findStudentByEmail(email);
+            if(studentOptional.isPresent()){
+                throw new IllegalStateException("Email already taken");
+            }
+            student.setEmail(email);
+        }
     }
 }
 
